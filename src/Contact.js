1b0b7668e3b777369168d0fb1d60d82022401dd8
samsher2578 <samsherkhan2578@gmail.com>
2023-10-20 17:50:05 +0530

    fixed

--- src/Contact.js
+++ src/Contact.js
@@ -18,17 +18,17 @@ import './Contact.css';
   };
 
   return (
-  <div>
+    <center>
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
       <label>Message</label>
-      <textarea name="message" cols="47" rows="10"/>
+      <textarea name="message" cols="40" rows="10"/>
       <input type="submit" value="Send" id='btn-sub' />
     </form>
-  </div>
+    </center>
   );
 };
 
