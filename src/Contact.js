cf86b338b1f3466547a2bc197a94f43f522883b2
samsher2578 <samsherkhan2578@gmail.com>
2023-10-20 18:01:58 +0530

    flexible

--- src/Contact.js
+++ src/Contact.js
@@ -2,21 +2,21 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import './Contact.css';
 
-    const Contact = () => {
-    const form = useRef();
-
-    const sendEmail = (e) => {
+const Contact = () => {
+  const form = useRef();
+  
+  const sendEmail = (e) => {
     e.preventDefault();
-
+    
     emailjs.sendForm('service_3962lvn', 'template_fgpztv7', form.current, 'eQ7Vq1Ik1K-XG1G-L')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
           console.log(error.text);
-      });
-      e.target.reset();
-  };
-
+        });
+        e.target.reset();
+      };
+      
   return (
     <center>
     <form ref={form} onSubmit={sendEmail}>
@@ -25,7 +25,7 @@ import './Contact.css';
       <label>Email</label>
       <input type="email" name="user_email" />
       <label>Message</label>
-      <textarea name="message" cols="40" rows="10"/>
+      <textarea name="message" cols="47" rows="10"/>
       <input type="submit" value="Send" id='btn-sub' />
     </form>
     </center>
